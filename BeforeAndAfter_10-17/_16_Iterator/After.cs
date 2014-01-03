80e613354ccde05af62c9d6a3b489e1909cc1340
Ido Barkan <barkanido@gmail.com>
2014-01-03 11:32:33 +0200

    Added last examples from class

---
+++ BeforeAndAfter_10-17/_16_Iterator/After.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace _16_Iterator
+{
+    public class After
+    {
+        public static void Run()
+        {
+            Country country = new Country();
+
+            /// the get_Cities property has been removed.
+            /// Client has to use Iterator.
+            IIterator it = country.CreateIterator();
+            while(it.MoveNext())
+            {
+                Console.Write(it.Current as string + ", ");
+            }
+
+
+            /// This demonstrates the abililty to create many iterators
+            /// and to iterate on them simultaniously
+            IIterator it1 = country.CreateIterator();
+            IIterator it2 = country.CreateIterator();
+
+            it1.MoveNext();
+            it1.MoveNext();
+            Console.WriteLine(it1.Current + Environment.NewLine);
+
+            Console.WriteLine("Two simultaneous iterations:");
+            while (it1.MoveNext() && it2.MoveNext())
+            {
+                Console.Write("[" + it1.Current + "]" + " " + it2.Current + " ");
+            }
+        }
+
+        /// The Aggregte interface
+        public interface IAggregate
+        {
+            IIterator CreateIterator();
+        }
+
+        /// The Iterator interface
+        public interface IIterator
+        {
+            bool MoveNext();
+            object Current { get; } /// polymorphic item reference
+            void Reset();
+        }
+
+        /// the ConcreteAggregate
+        public class Country : IAggregate
+        {
+            private readonly List<City> m_Cities;
+
+            public Country()
+            {
+                m_Cities = new List<City>()
+                {
+                    new City() {Name = "Tel Aviv", Prefix = "03", Area = 122.7f, Population = 1250000},
+                    new City() {Name = "Herzelia", Prefix = "09", Area = 35.17f, Population = 65200},
+                    new City() {Name = "Haifa", Prefix = "04", Area = 105.5f, Population = 1080000},
+                    new City() {Name = "Hadera", Prefix = "08", Area = 68.25f, Population = 225000}
+                };
+            }
+
+            public IIterator CreateIterator()
+            {
+                return new CityNamesIterator(this);
+            }
+
+            /// The ConcreteIterator:
+            /// This is a private nested class, which is tightly-coupled 
+            /// with the aggregate, and is specific to its needs.
+            private class CityNamesIterator : IIterator
+            {
+                private Country m_Agregate;
+                private int m_CurrentIdx = -1;
+                private int m_Count = -1;
+
+                public CityNamesIterator(Country i_Collection)
+                {
+                    m_Agregate = i_Collection;
+                    m_Count = m_Agregate.m_Cities.Count;
+                }
+
+                public void Reset()
+                {
+                    m_CurrentIdx = -1;
+                }
+
+                public bool MoveNext()
+                {
+                    if (m_Count != m_Agregate.m_Cities.Count)
+                    {
+                        throw new Exception("Collection can not be changed during iteration!");
+                    }
+                    if (m_CurrentIdx >= m_Count)
+                    {
+                        throw new Exception("Already reached the end of the collection");
+                    }
+
+                    return ++m_CurrentIdx < m_Agregate.m_Cities.Count;
+                }
+
+                public object Current
+                {
+                    get { return m_Agregate.m_Cities[m_CurrentIdx].Name; }
+                }
+            }
+        }
+
+        public class City
+        {
+            public string Name { get; set; }
+            public int Population { get; set; }
+            public string Prefix { get; set; }
+            public float Area { get; set; }
+        }
+    }
+}
---
+++ BeforeAndAfter_10-17/_17_Command/After.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _17_Command
+{
+    /// <summary>
+    ///  The 'after' demonstrates the classic implementation of the command pattern
+    ///  and makes the menu mechanism:
+    ///  a. Reusable (one can use the menu mechanism in a differnt program)
+    ///  b. Efficient (no need to go through a switch-case for each selection. just executing the selected command)
+    ///  c. Easy to maintain (in case of adding/removing menu items, we will only need to add/remove menu items)
+    ///  d. Not coupled with the client of the menu mechanism (the menu mechanism does not "know" the client and has no direct access to it's methods)
+    /// </summary>
+    public class After
+    {
+        public static void Run()
+        {
+            After after = new After();
+            after.InitMenu();
+            after.Execute();
+        }
+
+        Menu m_Menu;
+        public void InitMenu()
+        {
+            m_Menu = new Menu()
+            {
+                new MenuItem{Text = "Print Hello", Command = new PrintHelloCommand{Client = this}},
+                new MenuItem { Text = "Print Date", Command = new PrintDateCommand{ Client = this }},
+                new MenuItem { Text = "Print Time", Command = new PrintTimeCommand { Client = this }}
+            };
+        }
+
+        public void Execute()
+        {
+            m_Menu.Run();
+        }
+
+        private void PrintHello()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Hello!!");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        private void PrintTheDate()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(DateTime.Now.ToLongDateString());
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        private void PrintTheTime()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(DateTime.Now.ToLongTimeString());
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public class PrintHelloCommand : ICommand
+        {
+            public After Client { get; set; }
+
+            public void Execute()
+            {
+                Client.PrintHello();
+            }
+        }
+
+        public class PrintDateCommand : ICommand
+        {
+            public After Client { get; set; }
+
+            public void Execute()
+            {
+                Client.PrintTheDate();
+            }
+        }
+
+        public class PrintTimeCommand : ICommand
+        {
+            public After Client { get; set; }
+
+            public void Execute()
+            {
+                Client.PrintTheTime();
+            }
+        }
+    }
+
+    public interface ICommand
+    {
+        void Execute();
+    }
+
+    public class MenuItem
+    {
+        public ICommand Command { get; set; }
+        public string Text { get; set;}
+
+        public virtual void Selected()
+        {
+            if (Command != null)
+            {
+                Command.Execute();
+            }
+        }
+    }
+
+    public class Menu : List<MenuItem>
+    {
+        public void Run()
+        {
+            bool userQuit = false;
+            int userSelection;
+            while (!userQuit)
+            {
+                ShowMenu();
+                try
+                {
+                    userQuit = !GetUserSelection(out userSelection);
+                    if (!userQuit)
+                    {
+                        Console.Clear();
+                        this[userSelection - 1].Selected();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        private bool GetUserSelection(out int o_UserSelection)
+        {
+            bool userQuit = false;
+            string userSelection = Console.ReadLine();
+            if (userSelection.ToLower() != "q")
+            {
+                o_UserSelection = int.Parse(userSelection);
+                if (!(o_UserSelection > 0 && o_UserSelection <= this.Count))
+                {
+                    throw new ArgumentException(string.Format("You must select a number between 1-{0}", this.Count));
+                }
+            }
+            else
+            {
+                o_UserSelection = 0;
+                userQuit = true;
+            }
+
+            return !userQuit;
+        }
+
+        private void ShowMenu()
+        {
+            Console.WriteLine("Please choose:");
+
+            int itemNum = 1;
+            foreach (MenuItem item in this)
+            {
+                Console.Write(itemNum.ToString() + ": ");
+                Console.WriteLine(item.Text);
+                itemNum++;
+            }
+
+            Console.WriteLine(@"
+Type your selection number and press 'enter'.
+To quit type 'Q' and then 'enter'
+");
+        }
+    }
+}
